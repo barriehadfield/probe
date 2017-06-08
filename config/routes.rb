3a32b3d4aaa13364128101d6de1377e23cc3e0cc
Barrie Hadfield <barrie@workshare.com>
2017-06-08 13:25:19 +0100

    home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # mount Hyperloop::Engine => '/hyperloop'
+  root 'home#page'
+  match '*all', to: 'home#page', via: [:get]
 end
