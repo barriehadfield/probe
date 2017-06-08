a72e6eabcd66872e4a093ac775379e2bb68ad0a5
Barrie Hadfield <barrie@workshare.com>
2017-06-08 15:38:54 +0100

    with topbar

--- app/hyperloop/components/home.rb
+++ app/hyperloop/components/home.rb
@@ -1,16 +1,43 @@
 module Home
   class Page < Hyperloop::Component
     render(:div) do
-      div(class: 'container') {
-        div(class: 'row') {
-          div(class: 'col-md-3') {
-            "Filters"
+      DIV(class: 'container') {
+        TopBar()
+        BR()
+        DIV(class: 'row') {
+          DIV(class: 'col-md-3') {
+            Filters()
           }
-          div(class: 'col-md-9') {
-            "Cards" 
+          DIV(class: 'col-md-9') {
+            Cards()
           }
         }
       }
     end
   end
+
+  class TopBar < Hyperloop::Component
+    render do
+      NAV(class: 'navbar navbar-toggleable-sm fixed-top navbar bg-primary app-navbar') {
+        # A(class: 'navbar-brand', href: '#') { "Beat" }
+      }
+    end
+  end
+
+  class Filters < Hyperloop::Component
+    render do
+      DIV {
+        "Some Filters"
+      }
+    end
+  end
+
+  class Cards < Hyperloop::Component
+    render do
+      DIV {
+        "And some cards"
+      }
+    end
+  end
+
 end
