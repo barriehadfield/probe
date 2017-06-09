f8df3d0d6893e8b6e6f4173108e7327f471692c9
Barrie Hadfield <barrie@workshare.com>
2017-06-09 10:51:26 +0100

    styling

--- app/hyperloop/components/home.rb
+++ app/hyperloop/components/home.rb
@@ -1,34 +1,54 @@
 module Home
   class Page < Hyperloop::Component
     render(DIV) do
-      DIV(class: 'container') do
-        TopBar()
+      DIV(class: 'container') {
+        # TopBar()
         BR()
-        DIV(class: 'row') do
-          DIV(class: 'col-md-2') do
+        DIV(class: 'row') {
+          DIV(class: 'col-md-3') {
             Filters()
-          end
-          DIV(class: 'col-md-10') do
+          }
+          DIV(class: 'col-md-9') {
             Cards()
-          end
-        end
-      end
+          }
+        }
+      }
     end
   end
 
-  class TopBar < Hyperloop::Component
-    render(DIV) do
-      NAV(class: 'navbar navbar-toggleable-sm fixed-top navbar bg-primary app-navbar') do
-        # UL(class: 'nav navbar-nav') do
-         A(class: 'nav-link', href: '/') { "Beat" } 
-        # end
-      end
-    end
-  end
+  # class TopBar < Hyperloop::Component
+  #   render(DIV) do
+  #     NAV(class: 'navbar navbar-toggleable-sm fixed-top navbar bg-primary app-navbar') do
+  #       # UL(class: 'nav navbar-nav') do
+  #        A(class: 'nav-link', href: '/') { "Beat" }
+  #       # end
+  #     end
+  #   end
+  # end
 
   class Filters < Hyperloop::Component
+
+    def new_card_button
+      BUTTON(class: "btn btn-lg btn-danger") { "New Heart Card" }
+    end
+
+    def learn_button
+      BUTTON(class: "btn btn-link btn-info") { "Learn about H.E.A.R.T" }
+    end
+
     render do
-      DIV { "Some Filters" }
+      DIV(class: 'card') {
+        DIV(class: 'card-block') {
+          DIV(class: 'card-title') {
+            H5 { "Welcome Christine" }
+            P { "Lots of new things have happened since you were last here." }
+          }
+          DIV(class: 'text-center') {
+            new_card_button
+            learn_button
+          }
+        }
+      }
     end
   end
 
@@ -48,14 +68,14 @@ module Home
     end
 
     render do
-      DIV(class: 'card cursor-pointer') do
-        DIV(class: 'card-block') do
-          DIV(class: 'card-title') do
+      DIV(class: 'card cursor-pointer') {
+        DIV(class: 'card-block') {
+          DIV(class: 'card-title') {
             H4(class: "card-title") { "In-place editing in Transact will be super awsome!" }
-          end
+          }
           DIV(class: 'card-text') { "I am a Heart Card" }
-        end
-      end.on(:click) do
+        }
+      }.on(:click) do
         clicked
       end
     end
