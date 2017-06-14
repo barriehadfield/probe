3c66093e5ab082952028bff5bc1fc09b55a4173f
Barrie Hadfield <barrie@workshare.com>
2017-06-14 07:28:01 +0100

    perfect

--- app/hyperloop/components/probes.rb
+++ app/hyperloop/components/probes.rb
@@ -5,6 +5,13 @@ module Probes
     state show_modal: false
 
     render(DIV) do
+      summary_card.on(:click) do
+        mutate.show_modal !state.show_modal
+      end
+      modal if state.show_modal
+    end
+
+    def summary_card
       DIV(class: 'card cursor-pointer') {
         DIV(class: 'card-block') {
           DIV(class: 'card-title') {
@@ -12,42 +19,22 @@ module Probes
           }
           DIV(class: 'card-text') { "I am a Probe" }
         }
-      }.on(:click) do
-        mutate.show_modal !state.show_modal
-      end
-      ItemModal(show_modal: state.show_modal) if state.show_modal
-    end
-
-  end
-
-  class ItemModal < Hyperloop::Component
-
-    param :show_modal
-
-    render(DIV) do
-      modal
-    end
-
-    before_mount do
-    end
-
-    def close
-      puts "close"
+      }
     end
 
     def modal
       DIV {
-        Bs.Modal(show: true, dialogClassName: "modal-xl", onHide: lambda { close }) {
+        Bs.Modal(show: state.show_modal, dialogClassName: "modal-xl", onHide: lambda { close }) {
           Bs.ModalHeader {
-            # Modal.Modal.Title(id: 'ModalHeader') { "Here is the title " }
             H4 { "Header" }
           }
           Bs.ModalBody { P { "Body" } }
-          # Modal.Footer {
-          #   Modal.Modal.Dismiss(className: 'btn btn-default') { "Cancel"}
-          # }
         }
       }
+    end
+
+    def close
+      mutate.show_modal false
     end
   end
 
