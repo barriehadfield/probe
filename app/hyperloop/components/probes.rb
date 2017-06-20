2b5553969b9cad8b15f15dc3083bedf8d5183e20
Barrie Hadfield <barrie@workshare.com>
2017-06-20 11:06:41 +0100

    descriptive table

--- app/hyperloop/components/probes.rb
+++ app/hyperloop/components/probes.rb
@@ -140,12 +140,12 @@ module Probes
       Nav(tabs: true) {
         NavItem {
           NavLink( class: 'cursor-pointer', active: state.active_tab == 1, onClick: -> { mutate.active_tab 1 } ) {
-            "H.E.A.R.T"
+            "HEART"
           }
         }
         NavItem {
           NavLink( class: 'cursor-pointer', active: state.active_tab == 2, onClick: -> { mutate.active_tab 2 } ) {
-            "Config"
+            "Categories"
           }
         }
       }
@@ -164,43 +164,51 @@ module Probes
 
     def config_tab
       BR()
-      P { "Select the items you would like to track." }
+
+      P { "Choose one or more categories in the HEART framework that are the focus of your product or project." }
+      P { "Identifying clear goals will help choose the right metrics to help you measure progress." }
+      P { "Map your goals to lower-level signals. There are usually a large number of potentially useful signals for a particular goal." }
+      P { "Refine those signals into metrics you'll track over time or use in A/B testing. As in the previous step, there may be many possible metrics you could create from a given signal. It helps to have metrics that reflect the quality of the user experience, and that map to your main goals." }
+
       TABLE(class: 'table table-bordered') {
         THEAD {
           TR {
-            TH { "" }
-            TH { "Goal" }
+            TH { "Categories" }
+            TH { "Goals" }
             TH { "Signals" }
             TH { "Metrics" }
           }
         }
         TBODY {
           TR {
-            TD { "Happiness" }
-            TD { Button(size: :sm) {"Enabled"} }
+            TD {
+              SPAN { "Happiness - measures of user's attitudes."}
+              SPAN(class: 'pull-right') {Button(size: :sm) {"Enabled"}}
+            }
+            TD { "a" }
+            TD { "b" }
             TD { "c" }
-            TD { "d" }
           }
           TR {
-            TD { "Engagement" }
+            TD { "Engagement - level of user involvement." }
             TD { "b" }
             TD { "c" }
             TD { "d" }
           }
           TR {
-            TD { "Adoption" }
+            TD { "Adoption - gaining new users of a product or feature." }
             TD { "b" }
             TD { "c" }
             TD { "d" }
           }
           TR {
-            TD { "Retention" }
+            TD { "Retention - rate at which existing users are returning." }
             TD { "b" }
             TD { "c" }
             TD { "d" }
           }
           TR {
-            TD { "Task Success" }
+            TD { "Task Success - efficiency, effectiveness, and error rate." }
             TD { "b" }
             TD { "c" }
             TD { "d" }
