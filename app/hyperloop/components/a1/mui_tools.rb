9622019721a1dc5020a09bc02f07db6b617bda4e
Barrie Hadfield <barrie@workshare.com>
2017-06-29 06:21:07 +0100

    font card

--- app/hyperloop/components/a1/mui_tools.rb
+++ app/hyperloop/components/a1/mui_tools.rb
@@ -44,4 +44,21 @@ module MuiTools
     DIV(class: 'mdc-typography--caption') { yield }
   end
 
+  def font_card
+    Grid(fluid: true) {
+      Row { Col { Display1 { "Display1" } } }
+      Row { Col { Display2 { "Display2" } } }
+      Row { Col { Display3 { "Display3" } } }
+      Row { Col { Display4 { "Display4" } } }
+      Row { Col { Headline { "Headline" } } }
+      Row { Col { Title { "Title" } } }
+      Row { Col { SubHeading1 { "SubHeading1" } } }
+      Row { Col { SubHeading2 { "SubHeading2" } } }
+      Row { Col { Body1 { "Body1" } } }
+      Row { Col { Body2 { "Body2" } } }
+      Row { Col { Caption { "Caption" } } }
+      BR()
+    }
+  end
+
 end
